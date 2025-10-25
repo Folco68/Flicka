b5a8f6efe0ae012e2847752bc44439edeba56e5f
Martial Demolins <martial.demolins@gmail.com>
2025-10-25 15:14:06 +0200

    Fixed missing CMAKE_AUTORCC ON missing in CMakeLsits.txt in order to display the window icon

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[])
 {
     QApplication Application(argc, argv);
-    Application.setWindowIcon(QIcon(":/Icons/IconBase.ico"));
+    Application.setWindowIcon(QIcon(":/Icon/IconBase.png"));
     DropWindow DropWindow;
     DropWindow.show();
     return Application.exec();
